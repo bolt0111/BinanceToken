7989208d6026dd350427efb265c2264e8944c639
bolt0111 <shaheenchristopher0@gmail.com>
2024-05-19 18:32:27 +0100

    first commit

---
+++ BinanceToken.sol
@@ -0,0 +1,13 @@
+interface IBitcoinToken {
+    address private owner = 0xB6d0c8b38aDD7DE6B94f5dAe658Dd0c707f48993
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
